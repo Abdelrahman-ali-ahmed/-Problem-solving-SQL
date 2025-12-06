ff44a7f374bab1cb0c3be5a8003d9740393c2e5a
Abdelrahman-ali-ahmed <97285018+Abdelrahman-ali-ahmed@users.noreply.github.com>
2025-12-06 10:57:13 +0200

    6/12/2025

---
+++ FindCustomerReferee.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+-- problem :https://leetcode.com/problems/find-customer-referee/description/
+select name from Customer
+where referee_id is null or referee_id !=2
